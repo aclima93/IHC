f05520e34b172b063c60a627d4a81f6938c11c87
aclima93 <tocalagarto93@gmail.com>
2014-11-20 11:48:03 +0000

    - first commit -- what had been done previously

---
+++ Lesson1SAMPLE/Lesson1SAMPLE/KinectingTheDotsUserControl/KinectingTheDotsUserControl/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Media;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using Coding4Fun.Kinect.Wpf.Controls;
+using Microsoft.Research.Kinect.Nui;
+using System.IO;
+
+
+namespace KinectingTheDotsUserControl
+{
+    public partial class MainWindow : Window
+    {
+
+
+
+        Runtime runtime = Runtime.Kinects[0];
+
+        private static double _topBoundary;
+        private static double _bottomBoundary;
+        private static double _leftBoundary;
+        private static double _rightBoundary;
+        private static double _itemLeft;
+        private static double _itemTop;
+
+      public MainWindow()
+        {
+
+            //Runtime runtime = new Runtime();
+
+            //Runtime runtime;
+
+            
+
+
+            InitializeComponent();
+
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            Unloaded += new RoutedEventHandler(MainWindow_Unloaded);
+
+            Song1.Click +=new RoutedEventHandler(Song1_Click);
+            Song2.Click += new RoutedEventHandler(Song2_Click);
+
+            Song3.Click += new RoutedEventHandler(Song3_Click);
+            Song4.Click += new RoutedEventHandler(Song4_Click);
+
+            runtime.VideoFrameReady += runtime_VideoFrameReady;
+            runtime.SkeletonFrameReady += runtime_SkeletonFrameReady;
+
+        }
+
+        void Song1_Click(object sender, RoutedEventArgs e)
+        {
+
+                SoundPlayer correct = new SoundPlayer("tada.wav");
+                correct.Play();
+
+        }
+
+        void Song2_Click(object sender, RoutedEventArgs e)
+        {
+
+            SoundPlayer correct = new SoundPlayer("tada.wav");
+            correct.Play();
+
+        }
+
+        void Song3_Click(object sender, RoutedEventArgs e)
+        {
+
+            SoundPlayer correct = new SoundPlayer("tada.wav");
+            correct.Play();
+
+        }
+
+        void Song4_Click(object sender, RoutedEventArgs e)
+        {
+
+            SoundPlayer correct = new SoundPlayer("tada.wav");
+            correct.Play();
+
+        }
+      
+       
+        private static void CheckButton(HoverButton button, Ellipse thumbStick)
+        {
+
+
+            if (IsItemMidpointInContainer(button, thumbStick))
+            {
+                button.Hovering();
+            }
+            else
+            {
+                button.Release();
+            }
+        }
+
+       
+
+        public static bool IsItemMidpointInContainer(FrameworkElement container, FrameworkElement target)
+        {
+            FindValues(container, target);
+
+            if (_itemTop < _topBoundary || _bottomBoundary < _itemTop)
+            {
+                //Midpoint of target is outside of top or bottom
+                return false;
+            }
+
+            if (_itemLeft < _leftBoundary || _rightBoundary < _itemLeft)
+            {
+                //Midpoint of target is outside of left or right
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private static void FindValues(FrameworkElement container, FrameworkElement target)
+        {
+            var containerTopLeft = container.PointToScreen(new Point());
+            var itemTopLeft = target.PointToScreen(new Point());
+
+            _topBoundary = containerTopLeft.Y;
+            _bottomBoundary = _topBoundary + container.ActualHeight;
+            _leftBoundary = containerTopLeft.X;
+            _rightBoundary = _leftBoundary + container.ActualWidth;
+
+            //use midpoint of item (width or height divided by 2)
+            _itemLeft = itemTopLeft.X + (target.ActualWidth / 2);
+            _itemTop = itemTopLeft.Y + (target.ActualHeight / 2);
+        }
+
+
+        void runtime_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame skeletonSet = e.SkeletonFrame;
+
+            SkeletonData data = (from s in skeletonSet.Skeletons
+                                 where s.TrackingState == SkeletonTrackingState.Tracked
+                                 select s).FirstOrDefault();
+
+            if (data != null)
+            {
+                SetEllipsePosition(Head, data.Joints[JointID.Head]);
+
+                SetEllipsePosition(RightHand, data.Joints[JointID.HandRight]);
+                SetEllipsePosition(LeftHand, data.Joints[JointID.HandLeft]);
+
+                SetEllipsePosition(RightFoot, data.Joints[JointID.FootRight]);
+                SetEllipsePosition(LeftFoot, data.Joints[JointID.FootLeft]);
+            }
+
+
+
+            CheckButton(Song1, RightHand);
+            CheckButton(Song2, LeftHand);
+
+            CheckButton(Song3, RightFoot);
+            CheckButton(Song4, LeftFoot);
+        }
+
+
+        private void SetEllipsePosition(Ellipse ellipse, Joint joint)
+        {
+            Microsoft.Research.Kinect.Nui.Vector vector = new Microsoft.Research.Kinect.Nui.Vector();
+            vector.X = ScaleVector(800, joint.Position.X);
+            vector.Y = ScaleVector(600, -joint.Position.Y);
+            vector.Z = joint.Position.Z;
+
+            Joint updatedJoint = new Joint();
+            updatedJoint.ID = joint.ID;
+            updatedJoint.TrackingState = JointTrackingState.Tracked;
+            updatedJoint.Position = vector;
+
+            Canvas.SetLeft(ellipse, updatedJoint.Position.X);
+            Canvas.SetTop(ellipse, updatedJoint.Position.Y);
+        }
+
+        private float ScaleVector(int length, float position)
+        {
+            float value = (((((float)length) / 1f) / 2f) * position) + (length / 2);
+            if (value > length)
+            {
+                return (float)length;
+            }
+            if (value < 0f)
+            {
+                return 0f;
+            }
+            return value;
+        }
+
+        void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            runtime.Uninitialize();
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Since only a color video stream is needed, RuntimeOptions.UseColor is used.
+            runtime.Initialize(Microsoft.Research.Kinect.Nui.RuntimeOptions.UseColor | RuntimeOptions.UseSkeletalTracking);
+
+            //You can adjust the resolution here.
+            runtime.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+        }
+
+        void runtime_VideoFrameReady(object sender, Microsoft.Research.Kinect.Nui.ImageFrameReadyEventArgs e)
+        {
+            PlanarImage image = e.ImageFrame.Image;
+        
+            BitmapSource source = BitmapSource.Create(image.Width, image.Height, 0, 0,
+                PixelFormats.Bgr32, null, image.Bits, image.Width * image.BytesPerPixel);
+            videoImage.Source = source;
+        }
+
+
+    }
+}
